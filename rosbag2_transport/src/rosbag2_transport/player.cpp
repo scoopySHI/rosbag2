110ad7b68479f76f3cd8c7aff342bc68839a028a
Shi <Qiyao.Shi@streetscooter.com>
2020-02-17 22:54:06 +0100

    fix time generic method

--- rosbag2_transport/src/rosbag2_transport/player.cpp
+++ rosbag2_transport/src/rosbag2_transport/player.cpp
@@ -152,20 +152,153 @@ void hexdump(void *ptr, int buflen)
     }
   }
 
+//deal with string
+void Player::deal_with_string(const uint8_t *dds_buffer)
+{
+  std::cout << "STRING current position before change: " << current_position << "\n";
+  uint32_t length;
+  size_t string_header = sizeof(uint32_t);//string header 4 Bytes
+
+  unsigned long one_offset = string_header > last_data_size ? (string_header - current_position % string_header) & (string_header-1) : 0;
+  memcpy(&length, (dds_buffer+current_position+4+one_offset), string_header);
+  current_position = current_position + one_offset;
+  last_data_size = sizeof (char);
+  current_position += (string_header + length);
+  std::cout << "STRING length: " << length << "\n";
+  std::cout << "STRING current position--offset: " << current_position << "--" << one_offset << "\n";
+}
+
+//deal with wstring
+void Player::deal_with_wstring(const uint8_t *dds_buffer)
+{
+  std::cout << "WSTRING current position before change: " << current_position << "\n";
+  uint32_t length;
+  size_t string_header = sizeof(uint32_t);//wstring header 4 Bytes
+
+  unsigned long one_offset = string_header > last_data_size ? (string_header - current_position % string_header) & (string_header-1) : 0;
+  memcpy(&length, (dds_buffer+current_position+4+one_offset), string_header);
+  current_position = current_position + one_offset;
+  last_data_size = sizeof(uint32_t);
+  current_position += (string_header + length * (sizeof(uint32_t)));
+  std::cout << "WSTRING length: " << length << "\n";
+  std::cout << "WSTRING current position---offset: " << current_position << "--" << one_offset << "\n";
+}
+
+//find out the real offset of header in fastrtps serialized data
+void Player::calculate_position_with_align(const uint8_t * dds_buffer_ptr, const rosidl_typesupport_introspection_cpp::MessageMember *message_member, unsigned long stop_index)
+{
+  unsigned long one_offset = 0;
+  unsigned long data_size = 0;
+
+  for (unsigned int i=0; i < stop_index; i++) {
+    bool is_string = false;
+    bool is_wstring = false;
+    switch (message_member[i].type_id_) {
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_BOOL:
+        data_size = sizeof(bool);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_BYTE:
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_UINT8:
+        data_size = sizeof (uint8_t);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_CHAR:
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_INT8:
+        data_size = sizeof (char);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_FLOAT:
+        data_size = sizeof (float);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_FLOAT64:
+        data_size = sizeof (double);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_INT16:
+        data_size = sizeof (int16_t);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_UINT16:
+        data_size = sizeof (uint16_t);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_INT32:
+        data_size = sizeof (int32_t);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_UINT32:
+        data_size = sizeof (uint32_t);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_INT64:
+        data_size = sizeof (int64_t);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_UINT64:
+        data_size = sizeof (uint64_t);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_LONG_DOUBLE:
+        data_size = 8;
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_STRING:
+        deal_with_string(dds_buffer_ptr);
+        is_string = true;
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_WCHAR:
+        data_size = sizeof(uint16_t);
+        break;
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_WSTRING:
+        deal_with_wstring(dds_buffer_ptr);
+        is_wstring = true;
+        break; //wstring not finish
+      case ::rosidl_typesupport_introspection_cpp::ROS_TYPE_MESSAGE:
+        auto sub_members = static_cast<const rosidl_typesupport_introspection_cpp::MessageMembers *>(message_member[i].members_->data);
+        std::cout << "count: " << sub_members->member_count_ << "\n";
+        calculate_position_with_align(dds_buffer_ptr, sub_members->members_, sub_members->member_count_);
+        continue;
+      }
+    if(!is_string && !is_wstring && !message_member[i].is_array_)
+      {
+        std::cout << "NORMAL current position before change: " << current_position << "\n";
+        one_offset = data_size > last_data_size ? (data_size - current_position % data_size) & (data_size-1) : 0;
+        current_position = current_position + one_offset + data_size;
+        std::cout << "NORMAL current position--data size--offset: " << current_position << "--" << data_size << "--" << one_offset << "\n";
+        last_data_size = data_size;
+      }
+    else if(!is_string && !is_wstring && message_member[i].is_array_)
+      {
+        for (uint j = 0;j < message_member[i].array_size_; j++) {
+          std::cout << "ARRAY current position before change: " << current_position << "\n";
+          one_offset = data_size > last_data_size ? (data_size - current_position % data_size) & (data_size-1) : 0;
+          current_position = current_position + one_offset + data_size;
+          std::cout << "ARRAY current position--data size--offset: " << current_position << "--" << data_size << "--" << one_offset << "\n";
+          last_data_size = data_size;
+        }
+      }
+    else if (is_string && !is_wstring && message_member[i].is_array_)
+      {
+        for (uint j = 0;j < message_member[i].array_size_ - 1; j++) {
+          deal_with_string(dds_buffer_ptr);
+        }
+      }
+    else if (is_wstring && !is_string && message_member[i].is_array_)
+      {
+        for (uint j = 0;j < message_member[i].array_size_ - 1; j++) {
+        deal_with_wstring(dds_buffer_ptr);
+      }
+    }
+  }
+
+}
+
 void Player::play_messages_until_queue_empty()
 {
   ReplayableMessage message;
   while (message_queue_.try_dequeue(message) && rclcpp::ok()) {
     std::this_thread::sleep_until(start_time_ + message.time_since_start);
 
-    if ( topics_ts_map_.find(message.message->topic_name) != topics_ts_map_.end() ) {
+    if (topics_ts_map_.find(message.message->topic_name) != topics_ts_map_.end() ) {
       builtin_interfaces::msg::Time ros_time_to_set;
-      //auto time_ptr = std::make_shared<builtin_interfaces::msg::Time>(ros_time_to_set);
+      //auto time_ptr = std::make_shared<builtin_interfaces::msg::Time>(ros_time_to_set)
 
       std::chrono::time_point<std::chrono::system_clock> time_to_be_set = start_time_ + message.time_since_start;
-      auto offset_to_be_set = topics_ts_map_[message.message->topic_name];
+
+      auto offset_index = topics_ts_map_[message.message->topic_name].stop_index;
+      const rosidl_typesupport_introspection_cpp::MessageMember * msg_ptr = topics_ts_map_[message.message->topic_name].msg_member_ptr;
       //print offset to struct
-      std::cout << "offset in struct: " << offset_to_be_set << "\n";
+      //std::cout << "offset in struct: " << offset_to_be_set << "\n";
       std::chrono::duration_cast<std::chrono::nanoseconds>(time_to_be_set.time_since_epoch());
       //print chrono time
       std::cout << "chrono time: " << time_to_be_set.time_since_epoch().count() << "\n";
@@ -187,10 +320,22 @@ void Player::play_messages_until_queue_empty()
 
       //memcpy
       uint8_t * buffer_temp = message.message->serialized_data->buffer;
-      buffer_temp = buffer_temp + 4 + offset_to_be_set/2;
+      dds_buffer_ptr = message.message->serialized_data->buffer;
+      calculate_position_with_align(dds_buffer_ptr, msg_ptr, offset_index);
+      std::cout << "buffer_temp: " << (unsigned long)buffer_temp << "\n";
+      size_t header_time_sec_size = sizeof (int32_t);
+      unsigned long last_offset = header_time_sec_size > last_data_size ? (header_time_sec_size - current_position % header_time_sec_size) & (header_time_sec_size-1): 0;
+      current_position += last_offset;
+      buffer_temp = buffer_temp + current_position + 4; //plus dds header
+      std::cout << "current position--data size--last offset: " << current_position << "--" << header_time_sec_size << "--" << last_offset << "\n";
+      std::cout << "buffer_temp: " << (unsigned long)buffer_temp << "\n";
+
       memcpy(buffer_temp, &ros_time_to_set, sizeof(builtin_interfaces::msg::Time));
-      hexdump(message.message->serialized_data->buffer, sizeof(builtin_interfaces::msg::Time) + 24);
+      hexdump(message.message->serialized_data->buffer, sizeof(builtin_interfaces::msg::Time) + 48);
       std::cout << "--------------------------------------------\n";
+
+      current_position = 0;
+      last_data_size = ULONG_MAX;
     }
 
 //    if(options.clock_type == "current")
@@ -214,13 +359,25 @@ void Player::prepare_topic_ts_map()
       auto type_support = rosbag2::get_typesupport(topic.type, "rosidl_typesupport_introspection_cpp");
       auto msg_members = static_cast<const rosidl_typesupport_introspection_cpp::MessageMembers *>(type_support->data);
 
+      //test alignment
+      std::cout << "alignof Messagemember : " << alignof(rosidl_typesupport_introspection_cpp::MessageMember) << "\n";
+
       const rosidl_typesupport_introspection_cpp::MessageMember *msg_member_ptr = msg_members->members_;
       auto msg_member_count = msg_members->member_count_;
       for (unsigned int i = 0;i < msg_member_count;i++) {
           if(strcmp(msg_member_ptr[i].name_, "header") == 0)
           {
-              topics_ts_map_.insert(std::make_pair(topic.name, msg_member_ptr[i].offset_));
-              break;
+            //caculate header time stamper offset (int32----time.sec)
+//            current_position = calculate_position_with_align(msg_member_ptr, i);
+//            size_t header_time_sec_size = sizeof (int32_t);
+//            unsigned long one_offset = header_time_sec_size > last_data_size ? (header_time_sec_size - current_position % header_time_sec_size) : 0;
+//            unsigned long real_offset = one_offset + current_position;
+            //topics_ts_map_.insert(std::make_pair(topic.name, msg_member_ptr[i].offset_));
+            header_support_struct header_support;
+            header_support.stop_index = i;
+            header_support.msg_member_ptr = msg_member_ptr;
+            topics_ts_map_.insert(std::make_pair(topic.name, header_support));
+            break;
           }
       }
   }
@@ -228,7 +385,7 @@ void Player::prepare_topic_ts_map()
   //check map
   for(auto it = topics_ts_map_.begin(); it != topics_ts_map_.end(); it++)
   {
-    std::cout << "Name: " << it->first << "    header offset: " << it->second << "\n";
+    std::cout << "Name: " << it->first << "    header offset: " << it->second.stop_index << "\n";
   }
 
 }
--- rosbag2_transport/src/rosbag2_transport/player.hpp
+++ rosbag2_transport/src/rosbag2_transport/player.hpp
@@ -22,6 +22,7 @@
 #include <string>
 #include <cmath>
 #include <cstring>
+#include <climits>
 #include <unordered_map>
 
 #include "moodycamel/readerwriterqueue.h"
@@ -32,7 +33,9 @@
 
 
 #include "rosidl_typesupport_cpp/message_type_support.hpp"
+#include "rosidl_typesupport_introspection_cpp/field_types.hpp"
 #include "rosidl_typesupport_introspection_cpp/message_introspection.hpp"
+#include "rosidl_typesupport_introspection_c/message_introspection.h"
 
 using TimePoint = std::chrono::time_point<std::chrono::high_resolution_clock>;
 
@@ -43,6 +46,11 @@ class Reader;
 
 namespace rosbag2_transport
 {
+
+typedef struct{
+    int stop_index;
+    const rosidl_typesupport_introspection_cpp::MessageMember * msg_member_ptr;
+}header_support_struct;
 
 class GenericPublisher;
 class Rosbag2Node;
@@ -66,6 +74,9 @@ private:
   void prepare_clock(ReplayableMessage & message);
   void prepare_publishers();
   void prepare_topic_ts_map();
+  void calculate_position_with_align(const uint8_t * dds_buffer, const rosidl_typesupport_introspection_cpp::MessageMember *message_member, unsigned long stop_index);
+  void deal_with_string(const uint8_t * dds_buffer);
+  void deal_with_wstring(const uint8_t * dds_buffer);
 
   static constexpr double read_ahead_lower_bound_percentage_ = 0.9;
   static const std::chrono::milliseconds queue_read_wait_period_;
@@ -76,7 +87,12 @@ private:
   mutable std::future<void> storage_loading_future_;
   std::shared_ptr<Rosbag2Node> rosbag2_transport_;
   std::unordered_map<std::string, std::shared_ptr<GenericPublisher>> publishers_;
-  std::unordered_map<std::string, unsigned> topics_ts_map_;
+  //std::unordered_map<std::string, unsigned> topics_ts_map_;
+  std::unordered_map<std::string, header_support_struct> topics_ts_map_;
+
+  size_t last_data_size = ULONG_MAX;
+  unsigned long current_position = 0;
+  uint8_t *dds_buffer_ptr;
 };
 
 }  // namespace rosbag2_transport
